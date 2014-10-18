94fc5184ce8b0908aa84a16bf5592fbea0d8585e
Dale Reidy <dalebreidy@gmail.com>
2014-10-18 19:48:12 +0100

    Nested objects de-serialization tests

---
+++ src/Hatchet.Tests/HatchetConvertTests/NestedObjectTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Hatchet.Tests.HatchetConvertTests
+{
+    [TestFixture]
+    public class NestedObjectTests
+    {
+        class Inner
+        {
+            public string StrValue { get; set; }
+            public int IntValue { get; set; }
+        }
+
+        class Outer
+        {
+            public Inner Inner { get; set; }
+        }
+
+        [Test]
+        public void Deserialize_NestedObject_PropertiesArePopulated()
+        {
+            // Arrange
+            var input = "{ inner { strValue Hello intValue 1234} } }";
+
+            // Act
+            var result = HatchetConvert.Deserialize<Outer>(ref input);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Inner.Should().NotBeNull();
+            result.Inner.IntValue.Should().Be(1234);
+            result.Inner.StrValue.Should().Be("Hello");
+        } 
+    }
+}
