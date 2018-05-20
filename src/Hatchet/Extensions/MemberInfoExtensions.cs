3e4c47ffedc99a5e2b5040f8a1878ce2d495463f
Dale Reidy <dalebreidy@gmail.com>
2018-05-20 10:42:21 +0100

    Cache member attributes to reduce garbage

--- src/Hatchet/Extensions/MemberInfoExtensions.cs
+++ src/Hatchet/Extensions/MemberInfoExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -6,9 +7,25 @@ namespace Hatchet.Extensions
 {
     internal static class MemberInfoExtensions
     {
+        private static readonly Dictionary<MemberInfo, CustomAttributeData[]> Attributes =
+            new Dictionary<MemberInfo, CustomAttributeData[]>();
+        
         public static bool HasAttribute<T>(this MemberInfo memberInfo) where T : Attribute
         {
-            return memberInfo.GetCustomAttributes<T>().Any();
+            if (!Attributes.TryGetValue(memberInfo, out var values))
+            {                
+                values = memberInfo.CustomAttributes.ToArray();
+                Attributes[memberInfo] = values;
+            }
+
+            for (var index = 0; index < values.Length; index++)
+            {
+                var value = values[index];
+                if (value.AttributeType == typeof(T))
+                    return true;
+            }
+
+            return false;            
         }
     }
 }
